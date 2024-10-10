598e3fa3d3cd82604d648a6490fe48cc6dd49a61
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-10-10 13:52:42 +0700

    feat: add handlers for operator online/offline

---
+++ packages/tangle-subql/src/mappings/restake/operators/calls/handleGoOnline.ts
@@ -0,0 +1,26 @@
+import { SubstrateExtrinsic } from '@subql/types';
+import '@webb-tools/tangle-substrate-types';
+import assert from 'assert';
+import { Operator, OperatorStatus } from 'tangle-subql/types';
+import createOperatorStatusChange from 'tangle-subql/utils/createOperatorStatusChange';
+import getExtrinsicInfo from 'tangle-subql/utils/getExtrinsicInfo';
+
+export default async function handleGoOnline(extrinsic: SubstrateExtrinsic) {
+  const { signer, blockNumber } = getExtrinsicInfo(extrinsic);
+
+  const operator = await Operator.get(signer);
+
+  assert(operator, `Operator with ID ${signer} not found`);
+
+  operator.currentStatus = OperatorStatus.ACTIVE;
+  operator.lastUpdateAt = blockNumber;
+
+  const statusChange = createOperatorStatusChange(
+    signer,
+    blockNumber,
+    operator.id,
+    OperatorStatus.ACTIVE,
+  );
+
+  await Promise.all([operator.save(), statusChange.save()]);
+}
